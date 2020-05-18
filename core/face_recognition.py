63c0eb3c3b387dd1089b4942dc10da95b47ca8b7
alexa-perlov <56009415+alexa-perlov@users.noreply.github.com>
2020-05-18 00:24:57 -0400

    download img

--- core/face_recognition.py
+++ core/face_recognition.py
@@ -1,4 +1,9 @@
 import face_recognition
+from urllib import request
+from bs4 import BeautifulSoup
+import re
+import os
+import urllib
 
 
 def get_encoding(img):
@@ -8,4 +13,14 @@ def get_encoding(img):
 
 def mock(img):
     print(type(img))
+    url = img
+    response = request.urlopen(url).read()
+
+    soup= BeautifulSoup(response)
+    links = soup.find_all("a")
+
+    url_list = links[0]['href']
+    print(url_list)
+
+    request.urlretrieve(url_list, "face.jpg")
     return img
