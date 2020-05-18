9c4b64b71366d9490378a2875839496f072d3b56
James Ryan <jamesryan@Jamess-MBP-2.fios-router.home>
2020-05-17 23:34:27 -0400

    face_recognition

--- users/models.py
+++ users/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+from core.face_recognition import get_encoding
 
 
 class Profile(models.Model):
